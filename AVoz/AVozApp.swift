d6b86001fce56d8ac2eff6d09e27421f0dfd010d
Hyago Henrique <hyagoh@icloud.com>
2021-12-29 20:44:48 -0300

    Initial Commit

---
+++ AVoz/AVozApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVozApp.swift
+//  AVoz
+//
+//  Created by Hyago Henrique on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AVozApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
